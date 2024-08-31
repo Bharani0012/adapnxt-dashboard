a1186d3933fb8c83aff38c973c9ae0f034b88b3a
Bharanitharan <109167711+Bharani0012@users.noreply.github.com>
2024-08-31 11:39:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const MainContent = () => {
     <div className="col-10 main-section bg-secondary-subtle p-2">
       <div className='m-2'><span className='section-head'>{sectionTitle}</span></div>
       <Routes>
-        <Route exact path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route exact path="*" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/account" element={<Account />} />
         <Route path="/calculator" element={<Calculators />} />
